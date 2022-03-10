718b8beae0eba384acace93d71431357e0ac7875
kitagawakeita <k.syarin610@gmail.com>
2022-03-10 11:38:55 +0900

    stateのファイル移動

--- src/libs/state.ts
+++ src/libs/state.ts
@@ -1,5 +1,25 @@
-import type {Inputs} from 'src/interface/types'
+// import type { Inputs } from "src/interface/types";
 import { proxy } from "valtio";
+
+type Arr = {
+  name: string;
+  placeholder: string;
+  type: string;
+};
+
+export type Inputs = {
+  name: string;
+  email: string;
+  age: number | "";
+  content: string;
+};
+
+export const arr: Arr[] = [
+  { name: "お名前", placeholder: "名前", type: "text" },
+  { name: "メールアドレス", placeholder: "example@example.com", type: "text" },
+  { name: "年齢", placeholder: "", type: "number" },
+  { name: "内容", placeholder: "", type: "text" },
+];
 
 export const formState = proxy<Inputs>({
   name: "",
