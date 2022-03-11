90b3dee391203058a264c7f2983ef7808f5421c2
kitagawakeita <k.syarin610@gmail.com>
2022-03-11 23:38:25 +0900

    eslint反映

--- src/pages/complete.page.tsx
+++ src/pages/complete.page.tsx
@@ -1,19 +1,21 @@
 import { useRouter } from "next/router";
 import type { VFC } from "react";
-import { Layout } from "src/layouts/layout";
 
 const Complete: VFC = () => {
   const router = useRouter();
+
   return (
-    <Layout>
+    <div className="p-4 my-20 mx-auto w-80 border">
       <p className="my-8 text-center">送信が完了しました</p>
       <button
         className="block px-2 mx-auto bg-gray-200"
-        onClick={() => {return router.push("/")}}
+        onClick={() => {
+          return router.push("/");
+        }}
       >
         トップへ戻る
       </button>
-    </Layout>
+    </div>
   );
 };
 
